3ecaad7387bbabb49c3a35030270019dce7870e7
KellerJordan <kellerjordan1998@gmail.com>
2016-06-14 17:18:16 -0700

    solved problem 191

---
+++ Project Euler/euler191.py
@@ -0,0 +1,20 @@
+def euler(n):
+    r=2**n
+    r-=sumial(n)
+    p=0
+    while p<n:
+        q=n-p-1
+        r+=(2**p-sumial(p))*(2**q-sumial(q))
+        p+=1
+    return r
+sumials={}
+def sumial(n):
+    if n in sumials:
+        return sumials[n]
+    if n<3:
+        return 0
+    answer=2**(n-3)+sumial(n-3)+sumial(n-2)+sumial(n-1)
+    sumials[n]=answer
+    return answer
+
+print(euler(30))
